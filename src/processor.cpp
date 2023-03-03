c7cd86ab1d06e29c4a4afdde13b2ec3746ef8c27
Anass Baroudi <105128976+Manroody@users.noreply.github.com>
2023-03-02 22:46:43 -0500

    Update processor.cpp

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,7 +1,6 @@
 #include "processor.h"
 #include "linux_parser.h"
 
-// DONE: Return the aggregate CPU utilization
 float Processor::Utilization() {
     // cpu = (total - idle) / total
     float activeJiffies;
@@ -11,36 +10,3 @@ float Processor::Utilization() {
 
     return activeJiffies / totalJiffies;
 }
-
-
-// #include <fstream>
-// #include <sstream>
-// #include <string>
-// #include <vector>
-// #include <numeric>
-// // Above #includes were added by me
-// #include "processor.h"
-
-// using std::string; // Added by me
-
-// // Done: Return the aggregate CPU utilization
-// float Processor::Utilization() {
-//     string line;
-//     string value;
-//     float cpuSum;
-//     float downTime; // sum of idle, iowait, steal
-//     std::vector<float> cpuData;
-//     std::ifstream filestream("/proc/stat");
-//     if(filestream.is_open()) {
-//         std::getline(filestream, line);
-//         std::istringstream linestream(line);
-//         while(linestream >> value){
-//             if(value != "cpu")
-//                 cpuData.push_back(stof(value));
-//         }
-//         downTime = cpuData[3] + cpuData[4] + cpuData[7];
-//         cpuSum = std::accumulate(cpuData.begin(), cpuData.end(), 0.0);
-//     }
-
-//     return 1 - (downTime/cpuSum);
-// }
